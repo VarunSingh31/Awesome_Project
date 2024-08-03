fdd69b68a5bdaaa851c7345269fa30813cdb0a2d
Varun Singh <varunsingh@Varuns-MacBook-Air.local>
2024-08-03 17:57:00 +0530

    The voting system alert

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
 print("Hello")
+age = int(input("Enter the age of the person: "))
+
+if age >= 18:
+    print("The person is allowed to vote")
+
+else:
+    print("The person is not allowed to vote")
